abbc9244ddf21e748f572b396a4119d0a5d4a699
Kisoo <Kisoo@DESKTOP-CUI2F6B>
2020-06-16 16:50:25 +0900

    A에서 main 메서드 추가

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -3,5 +3,12 @@ public class A {
 	public static void main(String args[]) {
 		System.out.println("A: My Git!!");
 		System.out.println("A: My Git!!");
+		System.out.println("A: My Git!!");
+		
+		B myGit = new B();
+		myGit.sayGit();
+		
+		C myGit2 = new C();
+		myGit2.sayGit2();
 	}
 }
