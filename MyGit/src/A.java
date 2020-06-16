1fbc8fcc5b069b4cf27f0aeaec1b45d39c09a878
Kisoo <Kisoo@DESKTOP-CUI2F6B>
2020-06-16 16:47:40 +0900

    B 생성자 추가, A에서 B 생성자 테스트

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -3,5 +3,9 @@ public class A {
 	public static void main(String args[]) {
 		System.out.println("A: My Git!!");
 		System.out.println("A: My Git!!");
+		System.out.println("A: My Git!!");
+		
+		B myGit = new B();
+		myGit.sayGit();
 	}
 }
--- MyGit/src/B.java
+++ MyGit/src/B.java
@@ -1,6 +1,7 @@
 
 public class B {
-	public static void main(String args[]) {
+	public static void sayGit() {
+		System.out.println("B: My Git!!");
 		System.out.println("B: My Git!!");
 		System.out.println("B: My Git!!");
 	}
